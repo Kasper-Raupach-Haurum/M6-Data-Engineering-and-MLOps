77abfe0a9f90d197a392c5c3e88b831c9b6788ff
Md. Raiyan Alam <113442848+Raiyan1012@users.noreply.github.com>
2023-04-29 12:26:32 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st 
+
+from PIL import Image
+
+pickle_in = open("model.pkl","rb")
+model=pickle.load(pickle_in)
+
+
+#@app.route('/predict',methods=["Get"])
+def predict_HR(mae,rmse,r2):
+    
+    """Let's Authenticate the Attrition 
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: mae
+        in: query
+        type: number
+        required: true
+      - name: rmse
+        in: query
+        type: number
+        required: true
+      - name: r2
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+   
+    prediction=model.predict([[mae,rmse,r2]])
+    print(prediction)
+    return prediction
+
+
+
+def main():
+    st.title("HR Attrition")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    variance = st.text_input("mae","Type Here")
+    skewness = st.text_input("rmse","Type Here")
+    curtosis = st.text_input("r2","Type Here")
+    result=""
+    if st.button("Predict"):
+        result=predict_HR()
+    st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Lets LEarn")
+        st.text("Built with Streamlit")
+
+if __name__=='__main__':
+    main()
